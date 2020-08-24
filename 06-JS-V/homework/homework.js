b6529cb0766164fb5e124e2a12e9c31ecd46486b
laura <laura.aristegui@hotmail.com>
2020-08-24 18:35:53 -0300

    tareas V Y VI

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -7,14 +7,30 @@ function crearUsuario() {
   // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
-  // Tu código:
+  // Tu código: 
+        function Usuario (opciones) {
+          this.usuario = opciones.usuario;
+          this.nombre = opciones.nombre;
+          this.email = opciones.email;
+          this.password = opciones.password;
 
+        }
+          
+          Usuario.prototype.saludar = function() {
+
+            return 'Hola, mi nombre es ' + this.nombre;
+          }
+          return Usuario;
+         
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
-  // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
+  // El método debe llamarse "saludar" y debe devolver la strinlg "Hello World!"
   // Tu código:
+          Constructor.prototype.saludar = function() {
+              return "Hello World!";
+      }
 
 }
 
@@ -24,8 +40,26 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+  // Tu código: 
 
-}
+       String.prototype.reverse = function() {
+
+        var stringInvertida = '';
+
+        for( var i= this.length-1; i>=0; i--) {
+          stringInvertida = stringInvertida + this.charAt(i);
+
+        }
+          return stringInvertida;
+        }
+}   
+
+       
+      
+      
+
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,25 +1,44 @@
 // Do not change any of the function names
 
-function invocarCallback(cb) {
+function invocarCallback(cb) {     
   // Invoca al callback `cb`
-}
+    cb();
+} 
+
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  //numeros [1,2,3,4];
+      suma=0;
+      for( var i = 0; i < numeros.length; i++) {
+        suma = suma + numeros[i];
+      } 
+       cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+      array.forEach(function(elemento, index) {
+        cb(elemento);
+
+      });
 }
+
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+
+      var nuevoArray = array.map(function(elemento, index) {
+        return cb(elemento);
+      });
+        return nuevoArray;
 }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
